85f384a419667766a4c02e8d1cedf764b729b754
pparekh088 <parth.parekh088@gmail.com>
2017-02-08 23:12:00 -0500

    search wip

--- SearchPage.js
+++ SearchPage.js
@@ -75,8 +75,54 @@ let styles = StyleSheet.create({
 
 });
 
+function urlForQueryAndPage(key, value, pageNumber) {
+  var data = {
+      country: 'uk',
+      pretty: '1',
+      encoding: 'json',
+      listing_type: 'buy',
+      action: 'search_listings',
+      page: pageNumber
+  };
+  data[key] = value;
+ 
+  var querystring = Object.keys(data)
+    .map(key => key + '=' + encodeURIComponent(data[key]))
+    .join('&');
+ 
+  return 'http://api.nestoria.co.uk/api?' + querystring;
+};
+
 class SearchPage extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: 'london',
+      isLoading: false
+    };
+  }
+
+  onSearchTextChanged(event) {
+    console.log('onSearchTextChanged');
+    this.setState({ searchString: event.nativeEvent.text });
+    console.log(this.state.searchString);
+  }
+
+  _executeQuery(query) {
+    console.log(query);
+    this.setState({ isLoading: true });
+  }
+
+  onSearchPressed() {
+    var query = urlForQueryAndPage('place_name', this.state.searchString, 1);
+    this._executeQuery(query);
+  }
+
 	render(){
+    var spinner = this.state.isLoading ?
+  ( <ActivityIndicator
+      size='large'/> ) :
+  ( <View/>);
 		return(
 			<View style={styles.container}>
 			 <Text style={styles.description}>
@@ -85,20 +131,28 @@ class SearchPage extends Component{
 			 <Text style={styles.description}>
           Search by place-name, postcode or search near your location.
        </Text>
-      <View style={styles.flowRight}>
-      <TextInput
-        style={styles.searchInput}
-        placeholder='Search via name or postcode'/>
-      <TouchableHighlight style={styles.button}
+
+        <View style={styles.flowRight}>
+          <TextInput
+            style={styles.searchInput}
+            value= {this.state.searchString}
+            onChange={this.onSearchTextChanged.bind(this)}
+            placeholder='Search via name or postcode'/>
+
+          <TouchableHighlight 
+              onPress={this.onSearchPressed.bind(this)}
+              style={styles.button}
+              underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>Go</Text>
+          </TouchableHighlight>
+        </View>
+
+        <TouchableHighlight style={styles.LocationButton}
           underlayColor='#99d9f4'>
-        <Text style={styles.buttonText}>Go</Text>
-      </TouchableHighlight>
-      </View>
-      <TouchableHighlight style={styles.LocationButton}
-        underlayColor='#99d9f4'>
-      <Text style={styles.buttonText}>Location</Text>
-      </TouchableHighlight>    
-      <Image source={require('./resources/house.png')} style={styles.image}/> 
+        <Text style={styles.buttonText}>Location</Text>
+        </TouchableHighlight>   
+
+        <Image source={require('./resources/house.png')} style={styles.image}/> 
      	</View> 	
 		);
 	}
